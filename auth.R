67c648a2182cf051ccc034bd195fcf85ae63ace0
Arjun Mukundan <arjun.gm@gmail.com>
2014-11-10 10:28:01 -0600

    Create auth.R

---
+++ auth.R
@@ -0,0 +1,13 @@
+#Authorizing with credentials
+ 
+library(twitteR)
+ 
+api_key = "CONFIDENTIAL_API_KEY"
+ 
+api_secret = "CONFIDENTIAL_API_SECRET"
+ 
+access_token = "CONFIDENTIAL_ACCESS_TOKEN"
+ 
+access_token_secret = "CONFIDENTIAL_ACCESS_TOKEN_SECRET"
+ 
+setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
